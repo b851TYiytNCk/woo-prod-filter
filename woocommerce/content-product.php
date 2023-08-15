47266e52807f61728115eace59dd34ed65ee97b8
Vladyslav Nahornyi <highpassion90@gmail.com>
2023-08-15 08:52:51 +0000

    add initial woocommerce templates

---
+++ woocommerce/content-product.php
@@ -0,0 +1,98 @@
+
+<?php
+/**
+ * The template for displaying product content within loops
+ *
+ * This template can be overridden by copying it to yourtheme/woocommerce/content-product.php.
+ *
+ * HOWEVER, on occasion WooCommerce will need to update template files and you
+ * (the theme developer) will need to copy the new files to your theme to
+ * maintain compatibility. We try to do this as little as possible, but it does
+ * happen. When this occurs the version of the template file will be bumped and
+ * the readme will list any important changes.
+ *
+ * @see     https://docs.woocommerce.com/document/template-structure/
+ * @package WooCommerce\Templates
+ * @version 3.6.0
+ */
+
+defined( 'ABSPATH' ) || exit;
+
+global $product;
+
+// Ensure visibility.
+//if ( empty( $product ) || ! $product->is_visible() ) {
+	//return;
+//}
+
+?>
+
+<li <?php wc_product_class( '', $product ); ?>>
+	<?php
+	/**
+	 * Hook: woocommerce_before_shop_loop_item.
+	 *
+	 * @hooked woocommerce_template_loop_product_link_open - 10
+	 */
+	do_action( 'woocommerce_before_shop_loop_item' );
+
+	/**
+	 * Hook: woocommerce_before_shop_loop_item_title.
+	 *
+	 * @hooked woocommerce_show_product_loop_sale_flash - 10
+	 * @hooked woocommerce_template_loop_product_thumbnail - 10
+	 */
+	do_action( 'woocommerce_before_shop_loop_item_title' );
+
+	/**
+	 * Hook: woocommerce_shop_loop_item_title.
+	 *
+	 * @hooked woocommerce_template_loop_product_title - 10
+	 */
+	do_action( 'woocommerce_shop_loop_item_title' );
+
+	/**
+	 * Hook: woocommerce_after_shop_loop_item_title.
+	 *
+	 * @hooked woocommerce_template_loop_rating - 5
+	 * @hooked woocommerce_template_loop_price - 10
+	 */
+	do_action( 'woocommerce_after_shop_loop_item_title' );
+
+	/**
+	 * Hook: woocommerce_after_shop_loop_item.
+	 *
+	 * @hooked woocommerce_template_loop_product_link_close - 5
+	 * @hooked woocommerce_template_loop_add_to_cart - 10
+	 */
+	do_action( 'woocommerce_after_shop_loop_item' );
+
+
+	//print_r($template_args['order']);
+
+	if ( ! empty( $order ) && is_object($order)) {
+		$status = '<div class="status-wrap">';
+		$status .= '<p class="status-text">';
+
+		if ($order->get_status() == 'completed') {
+			$date = substr($order->get_date_completed(), 0, 10);
+
+			$status .= 'Доставлено ';
+			$status .= str_replace('-', '.',
+				$newDate = date("d-m-Y", strtotime($date))
+			);
+		} else {
+			$status .= __($order->get_status(), 'outletmax');
+		}
+
+		$status .= '</p>';
+		$status .= '</div>';
+	
+		echo $status;
+	}
+
+	?>
+</li>
+
+</nav>
+
